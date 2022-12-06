cc188726c0871c27f31444feec04358d0ba42269
Marissa Stiny <marissastiny@gmail.com>
2022-12-06 09:57:44 -0800

    added flow control

---
+++ flow_control/ruby_4.5.rb
@@ -0,0 +1,12 @@
+#When you run the following code...you get the following error message, why do you get this error and how can you fix it?
+#It needs another 'end' for the if statements
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end 
+end
+
+equal_to_four(5)
